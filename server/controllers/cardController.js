7f9f1d8a763b3488d328e15e791184f985e4331b
ryanmcd <rp.mcdan@gmail.com>
2021-03-06 14:08:00 -0500

    schema, middleware, routers V1

--- server/controllers/cardController.js
+++ server/controllers/cardController.js
@@ -1,3 +1,28 @@
+const models = require('../models/cardModels');
+
 const cardController = {};
+
+cardController.createCard = function(req, res, next){
+    //
+};
+
+cardController.findCard = function(req, res, next){
+    Card.find({}, 'question answer', (err, doc)=>{
+        if(err){
+            
+        } else {
+            res.locals = doc;
+            next();
+        }
+    })
+};
+
+cardController.updateCard = function(req, res, next){
+    //
+};
+
+cardController.deleteCard = function(req, res, next){
+    //
+};
 
 module.exports = cardController;
