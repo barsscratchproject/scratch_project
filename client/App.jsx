8f0f9e4d659333c0045948201384280b21f4b63b
Stacy <sslearn07@gmail.com>
2021-03-06 14:37:11 -0500

    set up containers and components pt 1

--- client/App.jsx
+++ client/App.jsx
@@ -1,4 +1,16 @@
 import React, { Component } from 'react';
+import Dashboard from './containers/Dashboard.jsx';
+
 import './styles.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>{<Dashboard />}</div>;
+  }
+}
 
 export default App;
