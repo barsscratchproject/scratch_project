de1261845d579b16a2766ffd62d3b2a7dfd0c6c3
ryanmcd <rp.mcdan@gmail.com>
2021-03-07 21:05:40 -0500

    new index card dashboard interface

--- client/components/DeckIcon.jsx
+++ client/components/DeckIcon.jsx
@@ -11,10 +11,10 @@ class DeckIcon extends Component {
     return (
       // <h1>THIS IS AN ICON</h1>
       <div className="singleDeckIcon">
-        <h1 className="oneDeck">{this.props.topic}</h1>
         <div className="quizBtnContainer">
           <button className="singleDeckQuizMeBtn">Quiz me!</button>
         </div>
+        <h1 className="oneDeck">{this.props.topic}</h1>
         <div className="editBtnContainer">
           <button className="singleDeckEditBtn">Edit</button>
           <button className="singleDeckDeleteBtn">Delete</button>
