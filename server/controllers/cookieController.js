a6ee67c8c77256a286334e615fcf43e4d5194861
Anthony <tonegiovanni@gmail.com>
2021-03-08 12:50:59 -0500

    updated cookieController.js and dataModel.txt

--- server/controllers/cookieController.js
+++ server/controllers/cookieController.js
@@ -1,7 +1,20 @@
+// Creating new user & logging in:
 // create user/successful login -> send cookie
-// > what kind of cookie?
-// > where? is it in session or local?
-// > what should I be adding to the cookie?
+// question 1 - hash function for storing a password which uses SALT? (lower priority)
+// question 2 - where are storing the cookie?
+// - local storage
+// - session storage
+// - another place ???
+// question 3 - what will the cookie look like?
+// - any specific info to add to the cookie?
+// - any options that are added to the option?
+// - any other pertinent questions
 
-// which middleware/functionality is only accessible only have a user has logged in?
-// how do we use the cookie above to verify that user is logged in?
+// User using the application
+// question 4 - which endpoints are only accessible after a user has logged in?
+// question 5 - how do we use the cookie above to verify that user is logged in?
+
+// User stops using the application
+// question 6 - how do we log a user out?
+// what happens to the cookie/s to lets the server know the browser is no longer the same client?
+// how do we invoke that functionality?
